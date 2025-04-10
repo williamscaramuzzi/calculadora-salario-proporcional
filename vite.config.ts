f01b5c6be8594b8985b5fec8a112ed8d5a5ae9a4
williamscaramuzzi <wsoficial1018@gmail.com>
2025-04-09 21:04:50 -0400

    Alterações nas configurações para gh pages

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,7 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react-swc'
 
-// https://vite.dev/config/
 export default defineConfig({
+  base: '/next-calculadoraproporcional/',
   plugins: [react()],
 })
